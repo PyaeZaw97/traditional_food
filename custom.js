9d5328931a3e84b68dfe667d36c5e2abd987f831
PyaeZaw97 <pyaephyozaw101@gmail.com>
2020-04-27 11:54:44 +0630

    update

--- custom.js
+++ custom.js
@@ -1,10 +1,4 @@
-
-
-
-		// $('#blogCarousel').carousel({
-		// 		interval: 5000
-		// });
-
+// Navbar//
  (function($) {
   "use strict";
 
@@ -51,78 +45,25 @@ $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
 	  });
 
 
-function initTimer()
-    {
-    	if($('.timer').length)
-    	{
-    		// Uncomment line below and replace date
-	    	var target_date = new Date("May 8, 2020").getTime();
-
-	    	// comment lines below
-	    	var date = new Date();
-	    	date.setDate(date.getDate() + 3);
-	    	var target_date = date.getTime();
-	    	//----------------------------------------
-	 
-			// variables for time units
-			var days, hours, minutes, seconds;
-
-			var d = $('#day');
-			var h = $('#hour');
-			var m = $('#minute');
-			var s = $('#second');
-
-			setInterval(function ()
-			{
-			    // find the amount of "seconds" between now and target
-			    var current_date = new Date().getTime();
-			    var seconds_left = (target_date - current_date) / 1000;
-			 
-			    // do some time calculations
-			    days = parseInt(seconds_left / 86400);
-			    seconds_left = seconds_left % 86400;
-			     
-			    hours = parseInt(seconds_left / 3600);
-			    seconds_left = seconds_left % 3600;
-			     
-			    minutes = parseInt(seconds_left / 60);
-			    seconds = parseInt(seconds_left % 60);
-
-			    // display result
-			    d.text(days);
-			    h.text(hours);
-			    m.text(minutes);
-			    s.text(seconds); 
-			 
-			}, 1000);
-    	}	
-    }
 
 
+// Event Timer//
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+let countDown = new Date('Dec 26, 2020 00:00:00').getTime(),
+    x = setInterval(function() {    
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+    }, second)
 
 
-$('.title').each(function(){
-  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-});
-
- anime.timeline({loop: true})
-  .add({
-    targets: '.title .letter',
-    translateY: [100,0],
-    translateZ: 0,
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 1400,
-    delay: function(el, i) {
-      return 300 + 30 * i;
-    }
-  }).add({
-    targets: '.title .letter',
-    translateY: [0,-100],
-    opacity: [1,0],
-    easing: "easeInExpo",
-    duration: 1200,
-    delay: function(el, i) {
-      return 100 + 30 * i;
-    }
-  });
